19647d29ffead0d7286e61875aacf6d4daa08eb2
MKerman <106352040+MKerman@users.noreply.github.com>
2022-08-12 16:50:51 -0400

    Fixed missing text & changed json link
    Line-by-line check for errors against:
    https://mbett.github.io/iwp/heroes-finished/scripts/heroes.js

--- mod/scripts/superheroes.js
+++ mod/scripts/superheroes.js
@@ -2,58 +2,49 @@ const header = document.querySelector('header');
 
 const section = document.querySelector('section');
 
-let requestURL='superheroes.json';
-
-let request= new XMLHttpRequest();
-
+let requestURL ='https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+let request = new XMLHttpRequest();
 request.open('GET', requestURL);
-
-request.responseType='json';
+request.responseType = 'json';
 request.send();
 
-request.onload= function() {
-  loaded
-  const superHeroes=request.response;
+request.onload = function() {
+  const superHeroes = request.response;
   populateHeader(superHeroes);
   showHeroes(superHeroes);
-  
+}
+
   function populateHeader(jsonObj) {
     
-    const myH1=document.createElement('h1');
-    
-   myH1.textContent =jsonObj['squadName'];
+    const myH1 = document.createElement('h1');
+    myH1.textContent = jsonObj['squadName'];
     header.appendChild(myH1);
     
-    const myPara=document.createElement('p');
-    myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
-    
-   header.appendChild(myPara);
+    const myPara = document.createElement('p');
+    myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];  
+    header.appendChild(myPara);
   }
   
   function showHeroes(jsonObj) {
+    const heroes = jsonObj['members'];
     
-    const heroes =jsonObj['members'];
-    
-    for (let i=0; i<heroes.length; ii) {
+    for (let i = 0; i<heroes.length; i++) {
+      const myArticle = document.createElement('article');
+      const myH2 = document.createElement('h2');
+      const myPara1 = document.createElement('p');
+      const myPara2 = document.createElement('p');
+      const myPara3 = document.createElement('p');
+      const myList = document.createElement('ul');
       
-      const myArticle=document.createElement('h2');
+      myH2.textContent = heroes[i].name;
+      myPara1.textContent = 'Secret identity: ';
+      myPara2.textContent = 'Age: ' + heroes[i].age;
+      myPara3.textContent = 'Superpowers:';
       
-      const myPara1=document.createElement('p');
-      const myPara2=document.createElement('p');
-      const myPara3=document.createElement('p');
-      
-      const myList=document.createElement('ul');
-      
-      myH2.textContent=heroes[i].name;
-      myPara1.textContent='Secret identity: ';
-      myPara2.textContent='Age: ' + heroes[i].age;
-      myPara3.textContent='Superpowers:';
-      
-      const superPowers= heroes[i].powers;
-        
-       for (let j=0; j < superPowers.length; j++) {
-           const listItem= document.createElement('li');
-            listItem.textContent =document.createElement('li');
+      const superPowers = heroes[i].powers;       
+       for (let j = 0; j < superPowers.length; j++) {
+           const listItem = document.createElement('li');
+            listItem.textContent = superPowers[j];
              myList.appendChild(listItem);
        }
          
