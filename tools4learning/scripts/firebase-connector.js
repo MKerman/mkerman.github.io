781aea8e3167f519d9baf356ba045a5da2719e49
MKerman <106352040+MKerman@users.noreply.github.com>
2022-08-18 19:22:55 -0400

    Create firebase-connector.js

---
+++ tools4learning/scripts/firebase-connector.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCEmGyTjzIqXMmOp05mHC-VLi0UeNh4Nw0",
+    authDomain: "tools4learning-d30bb.firebaseapp.com",
+    projectId: "tools4learning-d30bb",
+    storageBucket: "tools4learning-d30bb.appspot.com",
+    messagingSenderId: "609058748443",
+    appId: "1:609058748443:web:2ff009e342f835476920be",
+    measurementId: "G-EYMVVB95K5"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
