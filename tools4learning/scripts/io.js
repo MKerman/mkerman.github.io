57c96034be251d35647d954b384050a3ddca7636
MKerman <106352040+MKerman@users.noreply.github.com>
2022-08-18 20:35:57 -0400

    Update io.js

--- tools4learning/scripts/io.js
+++ tools4learning/scripts/io.js
@@ -3,6 +3,24 @@
 import {getDatabase, ref, set, update, child, get, onValue, remove } from
 "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js";
 
+//creat new flashcards
+
+let myFlashcards = [ {
+  "question": "What type of color is the building block for other colors?",
+  "image": "none",
+  "answer": "primary colors"
+   },
+   {
+   "question": "What colors do you get when you mix the primary colors together?",
+   "image"   : "none",
+   "answer"  : "secondary colors"  
+   },
+   {
+   "question": "What is the tool called to visualize the relationship between colors",
+   "image"  : "none",
+   }  
+];
+
 //Get pointer to database root
 const db = getDatabase();
 
@@ -14,5 +32,5 @@ const dbLocation = {};
 //store myFlashcards under flashcards in the database
 dbLocation['/deck/flashcards/'] = myFlashcards;
 
-//specify location in the dtabase tree
+//specify location in the database tree
 update (ref(db), dbLocation);
