0efeb78275ec47ceef0b11c9bbe6a0ef1dd0b91c
MKerman <106352040+MKerman@users.noreply.github.com>
2022-08-21 00:13:21 -0400

    create io js file

---
+++ mod12/scripts/io.js
@@ -0,0 +1,32 @@
+// import function to get database root, set a location and update the values
+
+import { getDatabase, ref, set, update, child, get, onValue, remove
+} from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js";
+
+
+let myFlashcards = [{
+		"question": "What are primary colors?",
+		"answer": "Red, Yellow, Blue"
+	},
+	{
+		"question": "What are secondary colors?",
+		"answer": "Orange, Green, Purple"
+	},
+	{
+		"question": "What are tertiary colors?",
+		"answer": "red-purple, blue-green and yellow-green"
+	}
+];
+
+//point to database root
+const db = getDatabase ();
+
+//make database point to location root & if location does not exist creat it
+
+const dbLocation = {};
+
+//write-overwrite 
+dbLocation['/deck/flashcards/'] = myFlashcards;
+
+//specify a location in database tree
+update (ref(db), dbLocation);
