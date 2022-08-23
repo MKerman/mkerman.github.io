778400a920669a4a1d1d3efa3414cb2bba0a92c0
MKerman <106352040+MKerman@users.noreply.github.com>
2022-08-23 00:24:23 -0400

    replace entire code
    updated array to include images, create if statements to display questions

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -1,32 +1,48 @@
-//define where the questions will appear on html
+//Define where the questions will appear on html
 const flashDeck = document.getElementById('flashDeck_div');
-flashDeck.innerHTML+= 'Are you ready?';
+flashDeck.innerHTML+= 'Are you ready?'+ '<br>';
+let questionCounter = 0;
+let questionDisplayed = false;
 
-//create class for objects
 class quizQ {
-  // Define names for value pairs in quizQ objects
-  constructor(question, answer) {
+  //Define name of value pairs in quizQ objects
+  constructor(question, answer, answphoto) {
     this.question = question;
     this.answer = answer;
+    this.answphoto = answphoto;
     this.correct = false;
-   } 
- 
+  }
 }
-// define empty array for myFlaschards
-let myFlashcard = []; 
+  
+// define empty array for flashcards
+let myFlashcard = [];
 
 //add objects to flashcard array
-myFlashcard.push(new quizQ('What sport are the Steelers?', 'football'));
-myFlashcard.push(new quizQ('What sport are the Penguins?', 'hockey'));
-myFlashcard.push(new quizQ('What sport are the Pirates?', 'baseball'));
-myFlashcard.push(new quizQ('What sport are the Riverhouse SC?', 'soccer'));
-myFlashcard.push(new quizQ('What sport are the Pittsburgh Forge?', 'rugby'));
+myFlashcard.push(new quizQ('What sport are the Steelers?', 'football', 'https://images.pexels.com/photos/2570139/pexels-photo-2570139.jpeg?cs=srgb&dl=pexels-jeandaniel-francoeur-2570139.jpg&fm=jpg'));
+myFlashcard.push(new quizQ('What sport are the Penguins?', 'hockey', 'https://images.pexels.com/photos/6847470/pexels-photo-6847470.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'));
+myFlashcard.push(new quizQ('What sport are the Pirates?', 'baseball', 'https://images.pexels.com/photos/1661950/pexels-photo-1661950.jpeg?cs=srgb&dl=pexels-steshka-willems-1661950.jpg&fm=jpg'));
+myFlashcard.push(new quizQ('What sport are the Riverhouse SC?', 'soccer', 'https://images.pexels.com/photos/114296/pexels-photo-114296.jpeg?cs=srgb&dl=pexels-markus-spiske-114296.jpg&fm=jpg'));
+myFlashcard.push(new quizQ('What sport are the Pittsburgh Forge?', 'rugby', 'https://images.pexels.com/photos/5116575/pexels-photo-5116575.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'));
 
-//create loop to print question pairs
-let i=0;
-while(i<myFlashcard.length){
-flashDeck.innerHTML+=myFlashcard[i].question + '<br/> ' + myFlashcard[i].answer + '<br/>';
-++i;
-
+//prints the entire question and answers on click
+document.querySelector('html').onclick=function(){
+  //resets flashDeck to display only the question on click
+  if (questionDisplayed === false) {
+      flashDeck.innerHTML=(myFlashcard[questionCounter].question +'<br>');
+     
+      //reset questionDisplayed
+      questionDisplayed = true;
+      }
+ else  { // display answer 
+      flashDeck.innerHTML+=('<br> '+ myFlashcard[questionCounter].answer +'<br>');
+      //display coresponding image for answer
+       flashDeck.innerHTML+=('<img src=\'' + myFlashcard[questionCounter].answphoto +'\' width=50%>')
+      //reset questionDisplayed
+      questionDisplayed = false;
+      //increment question counter
+      questionCounter++;
+ }
 }
 
+
+    
